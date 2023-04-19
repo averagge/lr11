9b5fda09be90ef031131b0793fa14cd07cc5a606
averagge <aleksejporfirev15@gmail.com>
2023-04-19 13:56:40 +0300

    Создал формы для регистрации авторизации и восстановления пароля

---
+++ lr11/lr11/Form2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lr11
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        public Form2(Form1 form1)
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
