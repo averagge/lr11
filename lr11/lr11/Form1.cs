d18a62008063fee10b46ea401f59ff508f715ab6
averagge <aleksejporfirev15@gmail.com>
2023-05-02 21:55:32 +0300

    Изменил Form1 и Form4

--- lr11/lr11/Form1.cs
+++ lr11/lr11/Form1.cs
@@ -40,7 +40,8 @@ namespace lr11
                     {
                         MessageBox.Show("Вход успешен!");
                         Form4 form4 = new Form4();
-                        form4.label1.Text = user.Login;
+                        form4.Login(user.Login, user.LastName, user.Name, user.Phone, user.Email, user.Birth,
+                            user.Design, user.DateFirst);
                         form4.Show();
                         form4.form1 = this;
                         this.Visible = false;
@@ -53,14 +54,18 @@ namespace lr11
         }
         private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         { 
-            Form2 form2 = new Form2(this);
+            Form2 form2 = new Form2();
             form2.Show();
+            form2.form1 = this;
+            this.Visible = false;
         }
 
         private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-            Form3 form3 = new Form3(this);
+            Form3 form3 = new Form3();
             form3.Show();
+            form3.form1 = this;
+            this.Visible = false;
         }
     }
 }
--- lr11/lr11/Form4.cs
+++ lr11/lr11/Form4.cs
@@ -12,15 +12,28 @@ namespace lr11
 {
     public partial class Form4 : Form
     {
-
+        public Form1 form1;
+        public void Login(string login, string lastname, string name, string phone, string email, 
+            string birth, string disign, string datefirst)
+        {
+            label1.Text = login;
+            label2.Text = lastname;
+            label3.Text = name;
+            label4.Text = phone;
+            label5.Text = email;
+            label6.Text = birth;
+            label7.Text = disign;
+            label8.Text = datefirst;
+        }
         public Form4()
         {
             InitializeComponent();
         }
 
-        private void Form4_Load(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
-
+            form1.Visible = true;
+            this.Close();
         }
     }
 }
